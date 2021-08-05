4cc4c8607974f913b6e980348ce0a59ce42a671f
Raymond Nazaryan <sudoheader@users.noreply.github.com>
2021-08-04 22:15:48 -0700

    added account and customer fields

--- src/com/dollarsbank/model/Account.java
+++ src/com/dollarsbank/model/Account.java
@@ -1,4 +1,12 @@
 package com.dollarsbank.model;
 
 public class Account {
+
+    private double depositAmount;
+    private double withdrawAmount;
+    private double fundsTransfer;
+    private String viewLastFiveTransactions;
+    private String displayInfo;
+
+
 }
--- src/com/dollarsbank/model/Customer.java
+++ src/com/dollarsbank/model/Customer.java
@@ -1,4 +1,79 @@
 package com.dollarsbank.model;
 
 public class Customer {
+    private String name;
+    private String address;
+    private String contact;
+    private String userId;
+    private String password;
+    private double initialAmount;
+
+    public Customer(String name, String address, String contact, String userId, String password, double initialAmount) {
+        this.name = name;
+        this.address = address;
+        this.contact = contact;
+        this.userId = userId;
+        this.password = password;
+        this.initialAmount = initialAmount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public double getInitialAmount() {
+        return initialAmount;
+    }
+
+    public void setInitialAmount(double initialAmount) {
+        this.initialAmount = initialAmount;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", contact='" + contact + '\'' +
+                ", userId='" + userId + '\'' +
+                ", password='" + password + '\'' +
+                ", initialAmount=" + initialAmount +
+                '}';
+    }
 }
