0d9731918cfd08521a21f53703e0af9da9fbad13
Raymond Nazaryan <sudoheader@users.noreply.github.com>
2021-08-08 22:26:53 -0700

    edits to ui handling

--- src/com/dollarsbank/application/DollarsBankApplication.java
+++ src/com/dollarsbank/application/DollarsBankApplication.java
@@ -1,5 +1,6 @@
 package com.dollarsbank.application;
 
+import com.dollarsbank.utility.ColorsUtility;
 import com.dollarsbank.utility.ConsolePrinterUtility;
 
 import java.util.Scanner;
@@ -10,16 +11,26 @@ public class DollarsBankApplication {
 
 //        ConsolePrinterUtility.printToConsole();
 
+        ConsolePrinterUtility.menu();
         Scanner scanner = new Scanner(System.in);
         int choice = scanner.nextInt();
+        System.out.print(ColorsUtility.RESET + choice);
 
         //TODO: based on userinput
         switch (choice) {
             case 1:
+                ConsolePrinterUtility.registration();
+//                try(String input = scanner.nextLine();) {
+//
+//                } catch () {
+//
+//                }
                 break;
             case 2:
+                ConsolePrinterUtility.credentials();
                 break;
             case 3:
+                ConsolePrinterUtility.exit();
                 break;
             default:
                 System.out.println("error");
--- src/com/dollarsbank/utility/ConsolePrinterUtility.java
+++ src/com/dollarsbank/utility/ConsolePrinterUtility.java
@@ -8,21 +8,19 @@ public class ConsolePrinterUtility implements ColorsUtility {
     static String welcomeCustomer = "WELCOME Customer!!!";
     static String recentTransactions = "5 Recent Transactions:";
 
-    public static void printToConsole() {
-
-//        System.out.println(ColorsUtility.BLUE + "BLUE COLOR");
-
-        printMessage(enterLogin);
-        printMessage(welcomeCustomer);
-        printMessage(recentTransactions);
-        
-        menu();
-        registration();
-        credentials();
-        authenticated();
-        transactions();
-        choice(); // will need a second one after login
-        userInput();
+//    public static void printToConsole() {
+//
+//        menu();
+//        registration();
+//        credentials();
+//        loggedIn();
+//        transactions();
+//        choice(); // will need a second one after login
+//        userInput();
+//    }
+    public static void exit() {
+        System.out.println("Goodbye!");
+        System.exit(0);
     }
 
     //FIXME: probably need to be public
@@ -30,35 +28,46 @@ public class ConsolePrinterUtility implements ColorsUtility {
         System.out.println(ColorsUtility.CYAN);
     }
 
+    public static void userChoice() {
+        System.out.print(ColorsUtility.RESET);
+    }
+
     public static void choice() {
-        System.out.println(ColorsUtility.RESET);
         System.out.println("1. Create New Account");
         System.out.println("2. Login");
-        System.out.println("3. Exit\n");
+        System.out.println("3. Exit");
         System.out.println(ColorsUtility.GREEN);
         System.out.println("Enter Choice (1,2, or 3) :");
+        userChoice();
+    }
+
+    public static void transactions() {
+        printMessage(recentTransactions);
+    }
+
+    public static void loggedIn() {
+        printMessage(welcomeCustomer);
+        System.out.println("1. Deposit Amount");
+        System.out.println("2. Withdraw Amount");
+        System.out.println("3. Funds Transfer");
+        System.out.println("4. View 5 Recent Transactions");
+        System.out.println("5. Display Customer Information");
+        System.out.println("6. Sign out\n");
+        System.out.println(ColorsUtility.GREEN);
+        System.out.println("Enter Choice (1,2,3,4,5 or 6) :");
         System.out.println(ColorsUtility.RESET);
     }
 
-    private static void transactions() {
-        //TODO: fill out the rest
-        System.out.println(ColorsUtility.RESET);
-    }
-
-    private static void authenticated() {
-        //TODO: fill out the rest
-        System.out.println(ColorsUtility.RESET);
-    }
-
-    private static void credentials() {
-        //TODO: fill out the rest
-        System.out.println(ColorsUtility.RESET);
+    public static void credentials() {
+        printMessage(enterLogin);
+        System.out.println("User Id :");
+        System.out.println("Password :");
         // when error, make it red
         System.out.println(ColorsUtility.RED + "Invalid Credentials. Try Again!");
     }
 
-    private static void registration() {
-        System.out.println(ColorsUtility.RESET);
+    public static void registration() {
+        System.out.println();
         printMessage(enterDetails);
         System.out.println("Customer Name:");
         System.out.println("Customer address:");
@@ -70,8 +79,6 @@ public class ConsolePrinterUtility implements ColorsUtility {
 
     public static void menu() {
         printMessage(welcome);
-        System.out.println(ColorsUtility.RESET);
-
         choice();
     }
 
@@ -86,5 +93,6 @@ public class ConsolePrinterUtility implements ColorsUtility {
             System.out.print("-");
         }
         System.out.println("+");
+        System.out.print(ColorsUtility.RESET);
     }
 }
